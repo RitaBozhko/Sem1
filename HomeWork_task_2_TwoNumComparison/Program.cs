a5353359f0813ddb09ef414a8427c8a19c2aadd5
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-25 17:54:02 +0300

    add HW task 2 Comparison of two numbers project

---
+++ HomeWork_task_2_TwoNumComparison/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, 
+//которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = 0;
+int b =0;
+
+Console.Write("Введите первое число: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = int.Parse(Console.ReadLine());
+
+if(a>b) Console.WriteLine($"Первое число больше второго и является максимальным.\nmax = {a}.");
+else if (a<b) Console.WriteLine($"Второе число больше первого и является максимальным.\nmax = {b}.");
+else Console.WriteLine("Числа равны.");
