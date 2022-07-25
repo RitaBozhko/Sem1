92a0131bf43379c5d76f4e0c79e351792fd3456b
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-25 17:21:13 +0300

    add task_3 Days of the Week Project

---
+++ task_3_DaysOfTheWeek/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 1;
+
+Console.Write("Какой сегодня по счёту день недели?\nВведите число от 1 до 7: ");
+
+bool userEnter = int.TryParse(Console.ReadLine(), out a);
+
+if(userEnter)
+    if(1 <= a && a <= 7)
+        if(a==1)
+            Console.WriteLine("Сегодня Понедельник.");
+        else if(a==2)
+            Console.WriteLine("Сегодня Вторник.");
+        else if(a==3)
+            Console.WriteLine("Сегодня Среда.");
+        else if(a==4)
+            Console.WriteLine("Сегодня Четверг.");
+        else if(a==5)
+            Console.WriteLine("Сегодня Пятница.");
+        else if(a==6)
+            Console.WriteLine("Сегодня Суббота.");
+        else
+            Console.WriteLine("Сегодня Воскресенье.");
+    else
+        Console.WriteLine("Ошибка ввода. Введите число от 1 до 7.");
+else
+    Console.WriteLine("Ошибка ввода. Введите целое число от 1 до 7.");
