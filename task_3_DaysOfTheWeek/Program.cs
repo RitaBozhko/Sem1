9a4cc7dd86726cd69c7754e2318c49f7dbd69a01
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-25 17:27:09 +0300

    add If condition shortened

--- task_3_DaysOfTheWeek/Program.cs
+++ task_3_DaysOfTheWeek/Program.cs
@@ -5,21 +5,20 @@ Console.Write("Какой сегодня по счёту день недели?\
 bool userEnter = int.TryParse(Console.ReadLine(), out a);
 
 if(userEnter)
-    if(1 <= a && a <= 7)
-        if(a==1)
-            Console.WriteLine("Сегодня Понедельник.");
-        else if(a==2)
-            Console.WriteLine("Сегодня Вторник.");
-        else if(a==3)
-            Console.WriteLine("Сегодня Среда.");
-        else if(a==4)
-            Console.WriteLine("Сегодня Четверг.");
-        else if(a==5)
-            Console.WriteLine("Сегодня Пятница.");
-        else if(a==6)
-            Console.WriteLine("Сегодня Суббота.");
-        else
-            Console.WriteLine("Сегодня Воскресенье.");
+    if(a==1)
+        Console.WriteLine("Сегодня Понедельник.");
+    else if(a==2)
+        Console.WriteLine("Сегодня Вторник.");
+    else if(a==3)
+        Console.WriteLine("Сегодня Среда.");
+    else if(a==4)
+        Console.WriteLine("Сегодня Четверг.");
+    else if(a==5)
+        Console.WriteLine("Сегодня Пятница.");
+    else if(a==6)
+        Console.WriteLine("Сегодня Суббота.");
+    else if (a==7)
+        Console.WriteLine("Сегодня Воскресенье.");
     else
         Console.WriteLine("Ошибка ввода. Введите число от 1 до 7.");
 else
