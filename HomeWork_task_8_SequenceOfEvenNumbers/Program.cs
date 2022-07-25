79de6d3be36393ca706791cf2a0606db040a5e00
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-25 20:01:04 +0300

    add HW task 8 Print sequence of even number from 1 to N Project

---
+++ HomeWork_task_8_SequenceOfEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int n = 0;
+
+Console.Write("Введите число: ");
+bool userEnter = int.TryParse(Console.ReadLine(), out n);
+
+if (userEnter)
+{
+    Console.WriteLine($"Чётные числа от 1 до {n}:");
+    while(a<=n)
+    {
+        Console.Write($"{a} ");
+        a += 2;
+    }
+}
+else Console.Write("Ошибка ввода.");
