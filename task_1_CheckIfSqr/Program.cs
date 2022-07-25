8e8f514b4adaed74a355cfe556766b05890aafe5
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-25 14:53:52 +0300

    add task_1 Check if one number is a square of another number

---
+++ task_1_CheckIfSqr/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 0;
+int b = 0;
+
+Console.Write("Введите первое число: ");
+bool enterA = int.TryParse(Console.ReadLine(), out a);
+
+Console.Write("Введите второе число: ");
+bool enterB = int.TryParse(Console.ReadLine(), out b);
+
+if (enterA && enterB)
+    if (a==b*b)
+        Console.WriteLine($"Да, число {a} является квадратом чиcла {b}.");
+    else
+        Console.WriteLine($"Нет, число {a} не явялется квадратом числа {b}.");
+else
+    Console.Write("Ошибка ввода! Пожалуйста, введите целые числа.");
